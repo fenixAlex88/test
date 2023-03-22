c78376912e7c93eebafb6debd5dc2dd6bb923bc0
Aleksey Pekar <fenix_al@tut.by>
2023-03-23 01:27:20 +0300

    fixed popup & pagenation

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,7 +1,9 @@
 import './Footer.scss';
 const Footer = () =>{
   return (
-    <header className='footer'></header>
+    <header className='footer'>
+      <div>&copy 2023 Aleksey Pekar</div>
+    </header>
   );
 }
 export default Footer;
